fa8553edcb166bbc0faac3a5914359b57b78ff5f
Joan Nicolau <joannicolau23@gmail.com>
2023-01-22 15:25:46 +0100

    insert with single statement, create eloquent

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,7 +6,6 @@ use App\Http\Requests\StorePostRequest;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -41,34 +40,39 @@ class PostController extends Controller
         $form_data = $request->all();  // get form data
 
         // 1) create and save Post model
-        $post = new Post;
-        $post->title = $form_data['title'];
-        $post->content = $form_data['content'];
-        $post->extract = $form_data['extract'];
-        $post->user_id = Auth::id();
-        $post->expirable = $form_data['expirable'] === 'on' ? true : false;
-        $post->comentable = $form_data['comentable'] === 'on' ? true : false;
-        $post->is_private = $form_data['access'] === 'private' ? true : false;
-        $is_saved = $post->save();
+        // $post = new Post;
+        // $post->title = $form_data['title'];
+        // $post->content = $form_data['content'];
+        // $post->extract = $form_data['extract'];
+        // $post->user_id = Auth::id();
+        // $post->expirable = $form_data['expirable'] === 'on' ? true : false;
+        // $post->comentable = $form_data['comentable'] === 'on' ? true : false;
+        // $post->is_private = $form_data['access'] === 'private' ? true : false;
+        // $is_saved = $post->save();
 
-        if ($is_saved) {
-            echo '<h3>Post saved successfully!</h3>';
-        } else {
-            echo '<h3>Cannot save post</h3>';
-        }
+        // if ($is_saved) {
+        //     echo '<h3>Post saved successfully!</h3>';
+        // } else {
+        //     echo '<h3>Cannot save post</h3>';
+        // }
 
         // 2) need fillable properties for mass assignment
         
-        // $post = Post::create([
-        //     'title' => $form_data['title'],
-        //     'content' => $form_data['content'],
-        //     'extract' => $form_data['extract'],
-        //     'user_id' => Auth::id(),
-        //     'expirable' => $form_data['expirable'] === 'on' ? true : false,
-        //     'comentable' => $form_data['comentable'] === 'on' ? true : false,
-        //     'is_private' => $form_data['access'] === 'private' ? true : false,
-        // ]);
-
+        try {
+            $post = Post::create([
+                'title' => $form_data['title'],
+                'content' => $form_data['content'],
+                'extract' => $form_data['extract'],
+                'user_id' => Auth::id(),
+                'expirable' => $form_data['expirable'] === 'on' ? true : false,
+                'comentable' => $form_data['comentable'] === 'on' ? true : false,
+                'is_private' => $form_data['access'] === 'private' ? true : false,
+            ]);
+            echo '<h3>Post saved successfully!</h3>';
+        } catch (Exception $e) {
+            echo '<h3>Cannot save post</h3>';
+        }
+ 
         // 3) QueryBuilder
 
         // 4) Sentencia raw
